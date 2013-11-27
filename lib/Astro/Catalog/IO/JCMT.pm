bc259074e94563e85e5d5e6868e4252128600537
Graham Bell <g.bell@jach.hawaii.edu>
2013-11-27 13:54:38 -1000

    Prepare v4.28 release
    Increased version number and added change record.  Increased
    version number of Astro::Catalog::IO::JCMT module.

--- lib/Astro/Catalog/IO/JCMT.pm
+++ lib/Astro/Catalog/IO/JCMT.pm
@@ -34,7 +34,7 @@ use base qw/ Astro::Catalog::IO::ASCII /;
 
 use vars qw/$VERSION $DEBUG /;
 
-$VERSION = '0.16';
+$VERSION = '0.17';
 $DEBUG   = 0;
 
 # Name must be limited to 15 characters on write
